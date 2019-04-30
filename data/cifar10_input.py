dc91446b18028492b7e3e2f59e1b2098c77095b7
konpanousis <46801533+konpanousis@users.noreply.github.com>
2019-04-30 12:04:38 +0300

    MInor changes

--- data/cifar10_input.py
+++ data/cifar10_input.py
@@ -27,7 +27,7 @@ import tensorflow as tf
 # Process images of this size. Note that this differs from the original CIFAR
 # image size of 32 x 32. If one alters this number, then the entire model
 # architecture will change and any model would need to be retrained.
-IMAGE_SIZE = 32
+IMAGE_SIZE = 28
 
 # Global constants describing the CIFAR-10 data set.
 NUM_CLASSES = 10
